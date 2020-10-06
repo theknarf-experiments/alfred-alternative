83d8528c15655342a8f1d95283d52dd44e447f3e
Frank Lyder Bredland <fl@nyggen.com>
2020-10-06 14:21:04 +0200

    Open and close webview

--- packages/woodhouse/src/main.rs
+++ packages/woodhouse/src/main.rs
@@ -1,4 +1,3 @@
-// Example copied from: https://github.com/Boscop/web-view
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
@@ -10,25 +9,28 @@ extern crate macos_hotkey;
 use web_view::*;
 use macos_hotkey::*;
 
+#[derive(Debug)]
+#[derive(PartialEq)]
+#[derive(Copy)]
+#[derive(Clone)]
+enum ViewVisible {
+    Visible,
+    Hidden,
+}
+
+static mut view_status : ViewVisible = ViewVisible::Hidden;
+static html_content : &str = include_str!("../../frontend/dist/index.html");
+
 unsafe extern "C" fn callback(
     _inHandlerCallRef: EventHandlerCallRef,
     _inEvent: EventRef,
     _inUserData: *mut ::std::os::raw::c_void
 ) -> OSStatus {
-    println!("hotkey");
-    let html_content = include_str!("../../frontend/dist/index.html");
-
-    web_view::builder()
-        .content(Content::Html(html_content))
-        .size(320, 480)
-        .resizable(false)
-        .frameless(true)
-        .debug(true)
-        .user_data(())
-        .invoke_handler(|_webview, _arg| Ok(()))
-        .run()
-        .unwrap();
-
+    match view_status {
+        ViewVisible::Visible => view_status = ViewVisible::Hidden,
+        ViewVisible::Hidden => view_status = ViewVisible::Visible,
+    }
+    println!("Hotkey {:?}", view_status);
 
     0
 }
@@ -36,17 +38,57 @@ unsafe extern "C" fn callback(
 fn main() {
     println!("Subscribing to hotkey cmd+space");
 
+    let mut prev_view_status = ViewVisible::Hidden;
+
+    let mut view : Option<web_view::WebView<_>> = None;
+
     unsafe {
         let (event, eventTarget) = SubHotkey(callback);
-        while ReceiveNextEvent(
-            0,
-            0 as *const EventTypeSpec,
-            kDurationForever as f64,
-            1,
-            &event
-        ) == noErr {
-            SendEventToEventTarget( event, eventTarget );
-            ReleaseEvent( event );
+        loop {
+            if ReceiveNextEvent(
+                0,
+                0 as *const EventTypeSpec,
+                kDurationForever as f64,
+                1,
+                &event
+            ) == noErr {
+                SendEventToEventTarget( event, eventTarget );
+                ReleaseEvent( event );
+            }
+
+            if prev_view_status != view_status {
+                match view_status {
+                    ViewVisible::Hidden => {
+                        match view {
+                            Some(ref mut iView) => iView.exit(),
+                            None => (),
+                        };
+                    },
+                    ViewVisible::Visible => {
+                        view = Some(web_view::builder()
+                            .content(Content::Html(html_content))
+                            .size(320, 480)
+                            .resizable(false)
+                            .frameless(true)
+                            .debug(true)
+                            .user_data(())
+                            .invoke_handler(|_webview, _arg| Ok(()))
+                            .build()
+                            .unwrap());
+                    },
+                }
+            }
+
+            prev_view_status = view_status;
+
+            if view_status == ViewVisible::Visible {
+                match view {
+                    Some(ref mut iView) => {
+                        iView.step();
+                    },
+                    None => (),
+                };
+            }
         }
     }
 }
