deb0095d5e750595c2921cd59be06ab8c4a9c9f8
Frank Lyder Bredland <fl@nyggen.com>
2021-04-11 16:08:16 +0200

    TypeScript setup

--- packages/frontend/webpack.config.js
+++ packages/frontend/webpack.config.js
@@ -2,9 +2,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
 
 module.exports = {
+	entry: './src/index.tsx',
 	module: {
 		rules: [
-			{ test: /\.jsx?$/, exclude: /node_modules/, use: 'babel-loader' }
+			{ test: /\.(js|ts|md)x?$/, exclude: /node_modules/, use: 'babel-loader' }
 		]
 	},
 	plugins: [
@@ -12,5 +13,8 @@ module.exports = {
 			inject: 'body'
 		}),
 		new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/./]),
-	]
+	],
+	resolve: {
+		extensions: ['.js', '.jsx', '.ts', '.tsx', '.mdx'],
+	},
 };
